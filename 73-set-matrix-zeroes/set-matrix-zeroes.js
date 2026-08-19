e72ab97da60facb641a720e3362b3038e9466940
M.Zakawat <zakiiwrites@gmail.com>
2026-08-19 14:13:29 +0500

    Time: 2 ms (85.09%) | Memory: 60.8 MB (27.08%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -0,0 +1,65 @@
+var setZeroes = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let firstRowZero = false;
+    let firstColZero = false;
+
+    // Check if first row contains 0
+    for (let j = 0; j < n; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+
+    // Check if first column contains 0
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+            break;
+        }
+    }
+
+    // Use first row and first column as markers
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Set marked rows to zero
+    for (let i = 1; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            for (let j = 1; j < n; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Set marked columns to zero
+    for (let j = 1; j < n; j++) {
+        if (matrix[0][j] === 0) {
+            for (let i = 1; i < m; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Zero out first row
+    if (firstRowZero) {
+        for (let j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Zero out first column
+    if (firstColZero) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+};
